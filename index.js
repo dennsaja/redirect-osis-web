8d0611fcd5fd6a19a58bd8c367445e17eba3911c
Denis <136689391+dennsaja@users.noreply.github.com>
2025-10-06 23:41:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const path = req.url === '/a' ? '' : req.url
+  const path = req.url === '/' ? '' : req.url
   const target = `https://osisgridaku.framer.ai${path}`
   res.writeHead(301, { Location: target })
   res.end()
